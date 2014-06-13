6a09af3f341a9987ba4f913c5d12a2ed17153025
山田かみら <superfishman9@gmail.com>
2014-06-13 08:57:40 +0800

    Update ukagaka.main.js

--- undone/ukagaka.main.js
+++ undone/ukagaka.main.js
@@ -7,11 +7,11 @@
 				//Google Sheet Key
 				GSK : '0ArRwmWo93u-mdG93a2dkSWxIbHEzZjRIeDdxZXdsU1E',
 				//Google Sheet Table
-                GST: "od6",
+				GST: "od6",
 				//Google Form Key
 				GFK : '1xADUIiBq1ksH7lxwSch1Nz_p2gSxdJttmv5OJOxJye0',
 				//Google Form Field
-                GFF : "entry.2030600456",
+				GFF : "entry.2030600456",
                 //millisecond
                 time: 3000
 			};
@@ -19,9 +19,43 @@
 			var options = $.extend(defaults, options);
 
 			return this.each(function(){
-				var o = options;
-
-				$.getJSON("", function(JData){});
+				var obj     = $(this);
+				var key     = options.GSK,
+					sheet   = options.GST,
+					formkey = options.GFK,
+					inputid = options.GFF;
+				var TalkList = [];
+				
+				function GetDataFromGoogleVisualization(pkey, target_col){
+					var query = new google.visualization.Query('//spreadsheets.google.com/tq?key='+ pkey +'&range='+ target_col);
+					query.send(function(response){
+						if(!response.isError()){
+							var JSONdata = JSON.parse(response.getDataTable().toJSON());
+							
+							for(var i = TalkList.length;i < JSONdata.rows.length; i++)
+								TalkList.push(JSONdata.rows[i].c[0].v);
+						}
+					});
+				};
+				
+				function CreatControlPanel(){
+					obj.append("<div id='U_Con_Panel'><span id='U_btn_display'>Display</span> | <span id='U_btn_menu>Menu</span></div>");
+				}
+				
+				function SetListener(){};
+				
+				function StringAdder(){};
+				
+				function ChangeMenu(){};
+				
+				function init(){};
+				
+				
+				
+				//$.getJSON("//spreadsheets.google.com/feeds/list/" + key + "/" + sheet + "/public/values?alt=json", function(JData){
+				//	for(var i = 0; i < JData.feed.entry.length; i++)
+				//		TalkString.push(JData.feed.entry[i].g)
+				//});
 
 			});
 		}
