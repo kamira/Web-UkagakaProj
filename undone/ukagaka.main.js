8be4bec9ca738aa8dc512eb4bb3f04eb170a18b1
山田かみら <superfishman9@gmail.com>
2014-06-15 05:42:56 +0800

    Update ukagaka.main.js

--- undone/ukagaka.main.js
+++ undone/ukagaka.main.js
@@ -2,7 +2,7 @@
 	$.fn.extend({
 		//plugin name
 		ukagaka: function(options) {
-
+		
 			var defaults = {
 				//Google Sheet Key
 				GSK : '0ArRwmWo93u-mdG93a2dkSWxIbHEzZjRIeDdxZXdsU1E',
@@ -12,12 +12,12 @@
 				GFK : '1xADUIiBq1ksH7lxwSch1Nz_p2gSxdJttmv5OJOxJye0',
 				//Google Form Field
 				GFF : "entry.2030600456",
-                //millisecond
+				//millisecond
 				time: 3000
 			};
-
+			
 			var options = $.extend(defaults, options);
-
+			
 			return this.each(function(){
 				var obj     = $(this);
 				var key     = options.GSK,
@@ -25,13 +25,15 @@
 					formkey = options.GFK,
 					inputid = options.GFF;
 				var TalkList = [];
+				var menujson = [];
 				
 				function GetDataFromGoogleVisualization(pkey, target_col){
 					var query = new google.visualization.Query('//spreadsheets.google.com/tq?key='+ pkey +'&range='+ target_col);
+					
 					query.send(function(response){
 						if(!response.isError()){
 							var JSONdata = JSON.parse(response.getDataTable().toJSON());
-							
+						
 							for(var i = TalkList.length;i < JSONdata.rows.length; i++)
 								TalkList.push(JSONdata.rows[i].c[0].v);
 						}
@@ -40,6 +42,12 @@
 				
 				function CreatControlPanel(){
 					obj.append("<div id='U_Con_Panel'><span id='U_btn_display'>Display</span> | <span id='U_btn_menu>Menu</span></div>");
+				}
+				
+				function LoadMenuJson(){
+					$.getJSON("string.json", function(JData){
+						
+					});
 				}
 				
 				function SetListener(){};
@@ -62,17 +70,29 @@
 					});
 				};
 				
-				function ChangeMenu(){};
+				function ChangeMenu(to){
+					switch($('menu').attr('data-menu')){
+						case 'main':
+							break;
+						case 'lear':
+							break;
+						case 'upda':
+							break;
+						case 'exit':
+							break;
+					}
+				};
 				
 				function init(){
 					var LoadGoogleApi=document.createElement('script');
-                    LoadGoogleApi.setAttribute("type","text/javascript");
+					LoadGoogleApi.setAttribute("type","text/javascript");
 					LoadGoogleApi.setAttribute("src", '//www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1"}]}');
 					
 					document.getElementsByTagName("head")[0].appendChild(LoadGoogleApi);
 					
 					GetDataFromGoogleVisualization(GSK,'B');
 					CreatControlPanel();
+					LoadMenuJson();
 				};
 				
 				
@@ -81,7 +101,7 @@
 				//	for(var i = 0; i < JData.feed.entry.length; i++)
 				//		TalkString.push(JData.feed.entry[i].g)
 				//});
-
+				
 			});
 		}
 	})
